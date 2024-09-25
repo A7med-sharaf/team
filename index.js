d90fff04cd0d3074f062b5733b4a9147d99eab7f
A7med-sharaf <a9986939@gmail.com>
2024-09-25 05:43:02 +0300

    ahmed

---
+++ index.js
@@ -0,0 +1,14 @@
+let r1=document.querySelector(".r1")
+let checked=false
+let show= ()=>{
+    if(r1 == false){
+       checked =true
+        console.log("Done");
+        
+    }else if(r1== true) {
+        checked =false
+        console.log("erro");
+        
+    }
+    
+}
